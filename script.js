db65deee0fc579cbfe49b8e7b9d61613f465e685
tommiller703755 <tom.miller703755@gmail.com>
2025-09-11 20:57:36 +0000

    Add js html and css code to create better gui
    this commit adds the code needed to further develop the js gameplay code
    so that its integrated directly with the html page and can be played
    through the page rather than the terminal. The game cannot be reset and
    only single player is possible

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 (function() {
+    // Create a delay function to be sued later
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
     // Create the game grid factory function
     function GameGrid() {
         console.log("Creating gameGrid");
+        // Initialize the grid
         grid = [];
         for (let i = 0; i < 9; i++) {
             grid.push(-1);
         }
 
+        // build the gameGrid object itself
         return {
             // Create the gamegrid varaibles
             grid: grid,
             gameOver: false,
+            playerTurnNow: true,
 
             // Create a function to print the grid to the console
             printGrid() {
@@ -33,7 +41,8 @@
             // Create a function to change one of the grid characters
             changeGrid(position, character) {
                 this.grid[position] = character;
-                this.printGrid();
+                this.detectVictory(character);
+                //this.printGrid();
             },
             detectVictory(character) {
                 let victory = false;
@@ -52,12 +61,14 @@
                         break;
                     }
                 }
-
+                
                 if (victory) {
-                    console.log(character + " has won!");
-                } else {
-                    console.log(character + " has not won.");
+                    const victoryMessage = document.getElementById("turn-tracker");
+                    if (character = "X") { victoryMessage.textContent = "The Player Wins!"; }
+                    else {victoryMessage.textContent = "The Computer Wins!"; }
                 }
+
+                this.gameOver = victory;
                 return victory;
                 
             },
@@ -68,39 +79,67 @@
                     position = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
                 }
                 this.changeGrid(position, "O");
-            },
-            playerTurn() {
-                let position = prompt("What grid square do you want to change?");
 
-                while (this.grid[position] != -1) {
-                    position = prompt("What grid square do you want to change?");
-                }
-                this.changeGrid(position, "X");
+                const cell = document.getElementById(position);
+                cell.classList.add("computer-cell");
+
+                console.log("Computer turn completed");
             },
-            turnLogic() {
-                let gameOver = false
+            playerTurn(position) {
+                this.changeGrid(position, "X");
+
+                const cell = document.getElementById(position);
+                cell.classList.add("player-cell");
+
+                console.log("Player turn completed");
+            },
+            async turnLogic(position) {
+                const turnText = document.getElementById("turn-tracker");
+                if (!this.gameOver) {
+                    this.playerTurn(position);
+
+                    // Check if the player won
+                    if (this.detectVictory("X")) {
+                        return;
+                    }
+
+                    turnText.textContent = "Computer's turn...";
+                    await delay(2000);
+                    this.computerTurn();
+
+                    if (this.detectVictory("O")) {
+                        return;
+                    }
+
+                    turnText.textContent = "Player's turn..."
+                }
             }
         }
     }
 
     // use the game grid factory function
-    grid = GameGrid();
+    gameGrid = GameGrid();
     //grid.turnLogic();
 
     // Dom manipulation scripts
     // Create the squares to fill in the main grid
     function generateDomGrid() {
         const grid = document.getElementById("grid");
+        grid.innerHTML = "";
         for (let i = 0; i < 3; i++) {
             const row = document.createElement("div");
             row.classList.add("grid-row");
             for (let j = 0; j < 3; j++) {
-                const cell = document.createElement("div");
+                const cell = document.createElement("button");
                 cell.classList.add("cell");
+                cell.id = (3 * i) + j;
 
                 // Create event listener for each of the grid cells
                 cell.addEventListener("click", () => {
-                    cell.style.backgroundColor = "blue";
+                    if (!cell.classList.contains("player-cell") && !cell.classList.contains("computer-cell")) {
+                        gameGrid.turnLogic(cell.id);
+                        //generateDomGrid();
+                    }
                 });
 
                 row.appendChild(cell);
